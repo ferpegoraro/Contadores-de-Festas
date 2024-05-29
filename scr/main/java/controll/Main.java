35e7d0abf20fad09968fc4e099be1b32c9b6d933
Fernando Pegoraro <fernandopbilia2304@gmail.com>
2024-05-28 23:53:42 -0300

    dfdfdf#

--- scr/main/java/controll/Main.java
+++
@@ -1,8 +0,0 @@
-package boundary;
-
-public class Main {
-    public static void main(String[] args) {
-        SistemaBoundary sistemaBoundary = new SistemaBoundary();
-        sistemaBoundary.iniciar();
-    }
-}
