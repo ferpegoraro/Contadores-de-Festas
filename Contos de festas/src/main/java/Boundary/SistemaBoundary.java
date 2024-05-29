d9158e1212f6e54fbcecf851e2f9246e11ab7184
mauricioGutierrezz <165610004+mauricioGutierrezz@users.noreply.github.com>
2024-05-29 00:24:33 -0300

    Update SistemaBoundary.java

--- Contos de festas/src/main/java/Boundary/SistemaBoundary.java
+++ Contos de festas/src/main/java/Boundary/SistemaBoundary.java
@@ -242,4 +242,170 @@ public class SistemaBoundary {
             System.out.println("Buffet: " + agendamento.getBuffet().getNome() + ", Data: " + dateFormat.format(agendamento.getData()) + ", Horário: " + agendamento.getHorario());
         }
     }
-   
+        private void cancelarAgendamento(Cliente cliente) {
+        System.out.println("Suas Reservas:");
+        List<Agendamento> agendamentos = sistema.getAgendamentosCliente(cliente);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        for (int i = 0; i < agendamentos.size(); i++) {
+            Agendamento agendamento = agendamentos.get(i);
+            System.out.println((i + 1) + ". Buffet: " + agendamento.getBuffet().getNome() + ", Data: " + dateFormat.format(agendamento.getData()) + ", Horário: " + agendamento.getHorario());
+        }
+        System.out.println("Digite o número da reserva que deseja cancelar ou reagendar:");
+        int opcaoReserva = scanner.nextInt();
+        scanner.nextLine();
+        if (opcaoReserva < 1 || opcaoReserva > agendamentos.size()) {
+            System.out.println("Opção inválida.");
+            return;
+        }
+        Agendamento agendamentoSelecionado = agendamentos.get(opcaoReserva - 1);
+        System.out.println("1. Cancelar reserva");
+        System.out.println("2. Reagendar reserva");
+        int opcaoAcao = scanner.nextInt();
+        scanner.nextLine();
+        if (opcaoAcao == 1) {
+            if (sistema.cancelarAgendamento(agendamentoSelecionado)) {
+                System.out.println("Reserva cancelada com sucesso!");
+            } else {
+                System.out.println("Falha ao cancelar reserva.");
+            }
+        } else if (opcaoAcao == 2) {
+            System.out.println("Selecione a nova data:");
+            List<String> datasDisponiveis = agendamentoSelecionado.getBuffet().getDatasDisponiveis();
+            for (int i = 0; i < datasDisponiveis.size(); i++) {
+                System.out.println((i + 1) + ". " + datasDisponiveis.get(i));
+            }
+            int opcaoData = scanner.nextInt();
+            scanner.nextLine();
+            if (opcaoData < 1 || opcaoData > datasDisponiveis.size()) {
+                System.out.println("Opção inválida.");
+                return;
+            }
+            String novaDataString = datasDisponiveis.get(opcaoData - 1);
+            Date novaData;
+            try {
+                novaData = dateFormat.parse(novaDataString);
+            } catch (ParseException e) {
+                System.out.println("Erro ao converter data.");
+                return;
+            }
+            
+            System.out.println("Selecione o novo horário:");
+            List<String> horariosDisponiveis = agendamentoSelecionado.getBuffet().getHorariosDisponiveis();
+            for (int i = 0; i < horariosDisponiveis.size(); i++) {
+                System.out.println((i + 1) + ". " + horariosDisponiveis.get(i));
+            }
+            int opcaoHorario = scanner.nextInt();
+            scanner.nextLine();
+            if (opcaoHorario < 1 || opcaoHorario > horariosDisponiveis.size()) {
+                System.out.println("Opção inválida.");
+                return;
+            }
+            String novoHorario = horariosDisponiveis.get(opcaoHorario - 1);
+            agendamentoSelecionado.setData(novaData);
+            agendamentoSelecionado.setHorario(novoHorario);
+            System.out.println("Reserva reagendada com sucesso para a data " + dateFormat.format(novaData) + " e horário " + novoHorario);
+        } else {
+            System.out.println("Opção inválida.");
+        }
+    }
+
+    private void avaliarBuffet(Cliente cliente) {
+        System.out.println("Avaliar Buffet");
+        
+        List<Agendamento> agendamentos = sistema.getAgendamentosCliente(cliente);
+        if (agendamentos.isEmpty()) {
+            System.out.println("Você não possui agendamentos para avaliar.");
+            return;
+        }
+        
+        System.out.println("Selecione o buffet que deseja avaliar:");
+        for (int i = 0; i < agendamentos.size(); i++) {
+            Agendamento agendamento = agendamentos.get(i);
+            System.out.println((i + 1) + ". Buffet: " + agendamento.getBuffet().getNome());
+        }
+        
+        int opcaoBuffet = scanner.nextInt();
+        scanner.nextLine();
+        
+        if (opcaoBuffet < 1 || opcaoBuffet > agendamentos.size()) {
+            System.out.println("Opção inválida.");
+            return;
+        }
+        
+        Agendamento agendamentoSelecionado = agendamentos.get(opcaoBuffet - 1);
+        
+        System.out.println("Digite a avaliação (0 a 5 estrelas):");
+        int avaliacao = scanner.nextInt();
+        scanner.nextLine();
+    
+        System.out.println("Avaliação registrada com sucesso!");
+    }
+
+    private void verSolicitacoesReserva(Buffet buffet) {
+        System.out.println("Solicitações de Reserva");
+        List<Agendamento> agendamentos = sistema.getAgendamentosBuffet(buffet);
+        for (Agendamento agendamento : agendamentos) {
+            System.out.println("Cliente: " + agendamento.getCliente().getNomeCompleto() + ", Data: " + new SimpleDateFormat("dd/MM/yyyy").format(agendamento.getData()) + ", Horário: " + agendamento.getHorario());
+        }
+    }
+
+    private void verAgendamentos(Buffet buffet) {
+        System.out.println("Agendamentos");
+        List<Agendamento> agendamentos = sistema.getAgendamentosBuffet(buffet);
+        for (Agendamento agendamento : agendamentos) {
+            System.out.println("Cliente: " + agendamento.getCliente().getNomeCompleto() + ", Data: " + new SimpleDateFormat("dd/MM/yyyy").format(agendamento.getData()) + ", Horário: " + agendamento.getHorario());
+        }
+    }
+
+    private void cancelarAgendamentoBuffet(Buffet buffet) {
+        System.out.println("Cancelar Agendamento");
+        verAgendamentos(buffet);
+        System.out.println("Digite o índice do agendamento para cancelar:");
+        int indice = scanner.nextInt();
+        scanner.nextLine();
+
+        List<Agendamento> agendamentos = sistema.getAgendamentosBuffet(buffet);
+        if (indice < 0 || indice >= agendamentos.size()) {
+            System.out.println("Índice inválido.");
+            return;
+        }
+
+        if (sistema.cancelarAgendamento(agendamentos.get(indice))) {
+            System.out.println("Agendamento cancelado com sucesso!");
+        } else {
+            System.out.println("Falha ao cancelar agendamento.");
+        }
+    }
+
+    private void avaliarCliente(Buffet buffet) {
+        System.out.println("Avaliar Cliente");
+        
+        List<Agendamento> agendamentos = sistema.getAgendamentosBuffet(buffet);
+        if (agendamentos.isEmpty()) {
+            System.out.println("Não há agendamentos para avaliar clientes.");
+            return;
+        }
+        
+        System.out.println("Selecione o agendamento do cliente que deseja avaliar:");
+        for (int i = 0; i < agendamentos.size(); i++) {
+            Agendamento agendamento = agendamentos.get(i);
+            System.out.println((i + 1) + ". Cliente: " + agendamento.getCliente().getNomeCompleto());
+        }
+        
+        int opcaoAgendamento = scanner.nextInt();
+        scanner.nextLine();
+        
+        if (opcaoAgendamento < 1 || opcaoAgendamento > agendamentos.size()) {
+            System.out.println("Opção inválida.");
+            return;
+        }
+        
+        Agendamento agendamentoSelecionado = agendamentos.get(opcaoAgendamento - 1);
+        
+        System.out.println("Digite a avaliação (0 a 5 estrelas):");
+        int avaliacao = scanner.nextInt();
+        scanner.nextLine();
+    
+        System.out.println("Avaliação registrada com sucesso!");
+    }
+}  
